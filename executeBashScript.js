df5851c27db5b92f52a3cb01b8c200dc360dc38a
zarif007 <zarifhuq786@gmail.com>
2023-09-03 21:38:59 +0600

    [ADDED]: execute using bash

--- executeBashScript.js
+++ executeBashScript.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { exec } from "child_process";
+import { spawnSync } from "child_process";
 import convertToBashScript from "./convertToBashScript.js";
 
 function executeBashScript(commands) {
@@ -8,22 +8,7 @@ function executeBashScript(commands) {
   // Write the Bash script to a temporary file
   fs.writeFileSync("mainScript.sh", bashScript);
 
-  // Make the script executable
-  exec("chmod +x mainScript.sh", (error) => {
-    if (error) {
-      console.error("Error making the script executable.");
-    } else {
-      // Execute the Bash script
-      exec("mainScript.sh", (error, stdout, stderr) => {
-        if (error) {
-          console.error("Error executing the script.");
-          console.error(stderr);
-        } else {
-          console.log(stdout);
-        }
-      });
-    }
-  });
+  spawnSync('bash', ['-c', bashScript], { stdio: 'inherit' });
 }
 
 export default executeBashScript;
--- mainScript.sh
+++ mainScript.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-echo -e "\e[30;46mRunning 'git pull'\e[0m"
-git pull
-echo -e "\e[30;46mRunning 'code .'\e[0m"
-code .
+echo -e "\e[30;46mRunning 'npm create vite@latest react-app -- --template react'\e[0m"
+npm create vite@latest react-app -- --template react
+echo -e "\e[30;46mRunning 'cd react-app'\e[0m"
+cd react-app
+echo -e "\e[30;46mRunning 'npm install -D tailwindcss postcss autoprefixer'\e[0m"
+npm install -D tailwindcss postcss autoprefixer
+echo -e "\e[30;46mRunning 'npx tailwindcss init -p'\e[0m"
+npx tailwindcss init -p
+echo -e "\e[30;46mRunning 'echo "/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}"
+'\e[0m"
+echo "/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}"
+
+echo -e "\e[30;46mRunning 'cd src && echo '@tailwind base; @tailwind components;  @tailwind utilities;' > index.css'\e[0m"
+cd src && echo '@tailwind base; @tailwind components;  @tailwind utilities;' > index.css
+echo -e "\e[30;46mRunning 'npm run dev'\e[0m"
+npm run dev
 read -n1 -r -p "Press any key to exit" key
